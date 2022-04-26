2c3a08fe67f6368a159d77a7082d9f0430f4e44d
Thuta Min Thway <thutaminthway2001@gmail.com>
2022-04-26 19:06:30 +0630

    vue components using typescript

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app">
-    hello, Vue 
+    I am {{name}}.
+    I am {{age}} Years old.
+    <button @click="changeName('kyaw kyaw')">
+      Change Name
+    </button> 
+    <button @click="changeAge(45)">
+      Change Name
+    </button> 
   </div>
 </template>
 
@@ -11,6 +18,20 @@ export default defineComponent({
   name: 'App',
   components: {
 
+  },
+  data(){
+    return{
+      name: 'Thuta',
+      age: 21 as number | string
+    }
+  },
+  methods: {
+    changeName(name: string){
+      this.name = name;
+    },
+    changeAge(age: number){
+      this.age = age;
+    }
   }
 });
 </script>
